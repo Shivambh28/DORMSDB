48ea768430681c41a033b85aa0887f626f18f55f
alexthemitchell <mitchell17@grinnell.edu>
2014-06-07 16:57:39 -0500

    Flesh out server.js, begin routes.js

---
+++ server/routes.js
@@ -0,0 +1,20 @@
+// routes.js
+
+Module.exports = function(app, Parse) {
+
+  // index
+
+  app.get('/', function(req,res){
+    res.end("index.html");
+  });
+
+
+
+
+
+
+
+
+
+
+}
---
+++ server/server.js
@@ -0,0 +1,38 @@
+// server.js
+
+// Setup
+var express = require('express');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+
+
+var https = require('https');
+var http = require('http');
+var app = express();
+var port = 8000;
+var Parse = require('parse').Parse;
+
+
+var auth = require('./auth.js');
+
+// Initialize Parse
+
+Parse.initialize(auth["parse-appid"], auth["parse-key"]);
+
+
+// configuration
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(expressSession({secret: auth["session-secret"]}));
+
+// Use routes
+var routes = (./routes.js)(app, Parse);
+
+// launch
+
+app.listen(port);
+console.log("Operating on port" + port);
