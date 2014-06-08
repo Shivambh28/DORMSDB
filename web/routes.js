334465a0280b2622409559d0766f73e9a83c9b7a
alexthemitchell <mitchell17@grinnell.edu>
2014-06-07 21:59:24 -0500

    Flesh out content delivery system

---
+++ web/routes.js
@@ -0,0 +1,62 @@
+// routes.js
+
+Module.exports = function(app, Parse) {
+
+  /* PAGES */
+
+  app.get('/', function(req,res){
+    res.sendfile("./public/index.html");
+  });
+
+  app.get('/halls/:hall', function(req,res){
+    res.end(req.params.hall);
+  });
+
+  app.get('/room/:room', function(req,res){
+    res.end("index.html");
+  });
+
+  app.get('/search/:query', function(req,res){
+    res.end("index.html");
+  });
+
+   app.get('/profile', function(req,res){
+    res.end("index.html");
+  });
+
+  app.get('/login', function(req,res){
+    res.end("index.html");
+    // res.redirect('back');
+  });
+
+  app.get('/logout', function(req,res){
+    res.end("index.html");
+  });
+
+  app.get('/signup', function(req,res){
+    res.end("index.html");
+  });
+
+  app.get('/api/:query', function(req,res){
+    res.json(null);
+  });
+
+
+  /* CONTENT DELIVERY */
+
+  app.get('/css/:page', function(req,res){
+    res.sendfile("./public/css" + req.params.page + ".css");
+  });
+
+  app.get('/js/:file', function(req,res){
+    res.sendfile("./public/js" + req.params.file + ".js");
+  });
+
+  app.get ('/image/:file'), function(req,res) {
+    res.sendfile("/public/images/" + file);
+
+};
+
+
+
+}
