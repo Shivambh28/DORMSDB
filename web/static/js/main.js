ac2860470bc8b4d1b1aa2f8193581ebdcc05a330
Shivam Bhalla <Shivam@Shivams-MacBook-Air.local>
2014-09-20 16:35:43 -0400

    homepage is almost done

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,3 +1,13 @@
 $(function() {
+	// scroll to search
+	$('.searchbtn').on('click', function(e) {
+		var target = $(this).data('target'),
+			offsetTop = $(target).offset().top;
+		
+		$('html, body').animate({
+			scrollTop : offsetTop - 90
+		});
 
+		e.preventDefault();
+	});
 });
