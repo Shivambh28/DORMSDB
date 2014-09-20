4e8e8b20399c8f3daece1e5b2942cd1591b8fcc0
alexthemitchell <mitchell17@grinnell.edu>
2014-09-20 09:48:08 -0400

    Add frame

--- web/cloud/cloud/main.js
+++ web/cloud/cloud/main.js
@@ -4,3 +4,11 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("createRoom", function(request, response){
+
+});
+
+Parse.Cloud.define("createUser", function(request, response){
+    response.success();
+});
